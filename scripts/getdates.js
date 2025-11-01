926869995b7d48dbf8db9d86a6019987686d4d3d
devdoris <devdoris1@gmail.com>
2025-11-02 00:51:17 +0100

    second commit.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
-// Put the current year in the footer
 document.getElementById("year-now").textContent = new Date().getFullYear();
 
-// Show the page's last update date
 document.getElementById("page-update").textContent =
   "Last updated: " + document.lastModified;
