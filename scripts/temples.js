f7eaf3d142fb33dbbf7756a88c85350bb645c55f
devdoris <devdoris1@gmail.com>
2025-11-08 19:47:27 +0100

    Fixed errors.

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,11 +7,14 @@ const temples = [
   { name: "Rome Italy Temple", location: "Rome, Italy", year: 2019, img: "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg" },
   { name: "Aba Nigeria Temple", location: "Aba, Nigeria", year: 2005, img: "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg" },
   { name: "Lima Peru Temple", location: "Lima, Peru", year: 1986, img: "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-temple/lima-peru-temple-12721-main.jpg" },
-  { name: "Paris France Temple", location: "Paris, France", year: 2017, img: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg" }
+  { name: "Paris France Temple", location: "Paris, France", year: 2017, img: "https://churchofjesuschristtemples.org/assets/img/temples/paris-france-temple/paris-france-temple-2056-main.jpg" },
+  { name: "Laie Hawaii Temple", location: "Laie, Hawaii", year: 1919, img: "https://churchofjesuschristtemples.org/assets/img/temples/laie-hawaii-temple/laie-hawaii-temple-7370-main.jpg"},
+  { name: "Mexico City Mexico Temple", location: "Mexico City, Mexico", year: 1983, img: "https://churchofjesuschristtemples.org/assets/img/temples/mexico-city-mexico-temple/mexico-city-mexico-temple-4060-main.jpg"},
+  { name: "Tokyo Japan Temple", location: "Tokyo, Japan", year: 1980, img: "https://churchofjesuschristtemples.org/assets/img/temples/tokyo-japan-temple/tokyo-japan-temple-26340-main.jpg"}
 ];
 
 const main = document.querySelector("main");
-const heading = document.querySelector("main h1");
+const heading = document.querySelector("h2");
 
 
 function showTemples(list) {
@@ -41,7 +44,7 @@ document.querySelectorAll("nav li").forEach(btn => {
     const type = btn.textContent.toLowerCase();
     let filtered = temples;
 
-    if (type === "old") filtered = temples.filter(t => t.year < 1900);
+    if (type === "old") filtered = temples.filter(t => t.year < 1920);
     if (type === "new") filtered = temples.filter(t => t.year > 2000);
     if (type === "large") filtered = temples.filter(t => ["Salt Lake Temple", "Rome Italy Temple"].includes(t.name));
     if (type === "small") filtered = temples.filter(t => ["Aba Nigeria Temple", "Lima Peru Temple"].includes(t.name));
